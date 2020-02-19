e859cae63d83e605550494ec236b3090737e2b75
icho-web <54773154+icho-web@users.noreply.github.com>
2020-02-19 15:47:44 +0500

    Учебный проект: в движении

---
+++ js/change-color.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function () {
+  var wizardCoat = document.querySelector('.wizard-coat');
+  var wizardEyes = document.querySelector('.wizard-eyes');
+  var wizardFireball = document.querySelector('.setup-fireball-wrap');
+  var coatColor = document.querySelector('.coat-color');
+  var eyesColor = document.querySelector('.eyes-color');
+  var fireballColor = document.querySelector('.fireball-color');
+
+  var changeBackgroundColor = function (element, arr, input) {
+    var randomValue = parseInt(window.utils.getRandomValue(0, arr.length), 10);
+    element.style.background = arr[randomValue];
+    element.style.fill = arr[randomValue];
+    input.value = arr[randomValue];
+  };
+
+  wizardFireball.addEventListener('click', function () {
+    changeBackgroundColor(wizardFireball, window.utils.FIREBALLS_COLOR, fireballColor);
+  });
+
+  wizardCoat.addEventListener('click', function () {
+    changeBackgroundColor(wizardCoat, window.utils.COAT_COLOR, coatColor);
+  });
+
+  wizardEyes.addEventListener('click', function () {
+    changeBackgroundColor(wizardEyes, window.utils.EYES_COLOR, eyesColor);
+  });
+})();
