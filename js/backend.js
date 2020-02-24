01b03faa215c9595fa52b223e432e0c709410c83
icho-web <54773154+icho-web@users.noreply.github.com>
2020-02-24 19:23:07 +0500

    правки

--- js/backend.js
+++ js/backend.js
@@ -1,46 +1,59 @@
 'use strict';
 
 (function () {
+  var URL = 'https://js.dump.academy/code-and-magick';
+  var serverTime = 10000;
+  var statusOk = 200;
+
+  var setup = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === statusOk) {
+        onLoad(xhr.response);
+      } else {
+        onError(xhr.response);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = serverTime;
+
+    return xhr;
+  };
+
+  function load(onLoad, onError) {
+    var xhr = setup(onLoad, onError);
+    xhr.open('GET', URL + '/data');
+    xhr.send();
+  }
+
+  function save(data, onLoad, onError) {
+    var xhr = setup(onLoad, onError);
+    xhr.open('POST', URL);
+    xhr.send(data);
+  }
+
+  function errorHandler(errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 5px auto; text-align: center; background-color: red; border: 2px solid black';
+    node.style.position = 'fixed';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  }
+
   window.backend = {
-    load: function (onLoad, onError) {
-      var URL = 'https://js.dump.academy/code-and-magick/data';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = 10000;
-
-      xhr.open('GET', URL);
-      xhr.send();
-    },
-    save: function (data, onLoad, onError) {
-      var URL = 'https://js.dump.academy/code-and-magick';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.open('POST', URL);
-      xhr.send(data);
-    }
+    load: load,
+    save: save,
+    errorHandler: errorHandler
   };
 })();
--- js/renderWizard.js
+++ js/renderWizard.js
@@ -36,18 +36,6 @@
     evt.preventDefault();
   });
 
-  var errorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-    node.style.position = 'absolute';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '30px';
-
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
-  };
-
-  window.backend.load(renderWizard, errorHandler);
+  window.backend.load(renderWizard, window.backend.errorHandler);
 
 })();
